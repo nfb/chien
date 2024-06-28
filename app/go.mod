62a5efb14753bd5a7e548f67dc9d07f1608101b2
nic <nicolas@nfbush.eu>
2024-06-29 00:25:18 +0100

    initial commit

---
+++ app/go.mod
@@ -0,0 +1,3 @@
+module github.com/nfb/chien/app
+
+go 1.22.3
---
+++ app/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log/slog"
+	"net/http"
+)
+
+func catchall(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r)
+	w.Write([]byte("meow"))
+	slog.Info("Caught Request")
+}
+
+func main() {
+	fmt.Println("Starting...")
+	http.HandleFunc("/*", catchall)
+	http.ListenAndServe(":3000", nil)
+}
