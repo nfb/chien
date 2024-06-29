908fc6bf174df678f7281f12eaedb1e3da89774e
nic <nicolas@nfbush.eu>
2024-06-29 02:55:43 +0100

    Configurable loglevels and bindport

--- app/main.go
+++ app/main.go
@@ -5,7 +5,10 @@ import (
 	"io"
 	"log/slog"
 	"net/http"
+	"os"
 )
+
+var BINDADDR string = ":3000"
 
 func catchall(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(r)
@@ -33,12 +36,36 @@ func interactionHandler(w http.ResponseWriter, r *http.Request) {
 	slog.Info("Caught Request")
 }
 
+func configureLogging() {
+	var programLevel = new(slog.LevelVar) // Info by default
+	h := slog.NewJSONHandler(os.Stderr, &slog.HandlerOptions{Level: programLevel})
+	slog.SetDefault(slog.New(h))
+
+	switch loglevel := os.Getenv("LOGLEVEL"); loglevel {
+	case "DEBUG":
+		programLevel.Set(slog.LevelDebug)
+	case "INFO":
+		programLevel.Set(slog.LevelInfo)
+	case "WARN":
+		programLevel.Set(slog.LevelWarn)
+	case "ERROR":
+		programLevel.Set(slog.LevelError)
+	default:
+		programLevel.Set(slog.LevelInfo)
+	}
+}
+
 func main() {
-	fmt.Println("Starting...")
+	envBindAddr = os.Getenv("BINDADDR")
+	if envBindAddr != "" {
+		BINDADDR = envBindAddr
+	}
+	fmt.Println("Starting server, binding to " + BINDADDR)
 	http.HandleFunc("/*", catchall)
 	http.HandleFunc("/api/interactions", interactionHandler)
-	err := http.ListenAndServeTLS(":443", "cert.pem", "key.pem", nil)
+	err := http.ListenAndServeTLS(BINDADDR, "cert.pem", "key.pem", nil)
 	if err != nil {
 		slog.Error("TLS Server returned error: " + err.Error())
 	}
+
 }
