37b536eba587a62043e38b1e301239c97387aaf9
nic <nicolas@nfbush.eu>
2024-06-29 01:56:27 +0100

    add pong and debug logging

--- app/main.go
+++ app/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log/slog"
 	"net/http"
 )
@@ -12,9 +13,30 @@ func catchall(w http.ResponseWriter, r *http.Request) {
 	slog.Info("Caught Request")
 }
 
+func interactionHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		bodyb, err := io.ReadAll(r.Body)
+		if err != nil {
+			slog.Error("Failed to read body: " + err.Error())
+			http.Error(w, "Serverr", 500)
+			return
+		}
+		body := string(bodyb)
+		slog.Debug("Received Body: " + body)
+		if body == "PING" {
+			w.Write([]byte("PONG"))
+			return
+		}
+	}
+	fmt.Println(r)
+	w.Write([]byte("meow"))
+	slog.Info("Caught Request")
+}
+
 func main() {
 	fmt.Println("Starting...")
 	http.HandleFunc("/*", catchall)
+	http.HandleFunc("/api/interactions", interactionHandler)
 	err := http.ListenAndServeTLS(":3000", "cert.pem", "key.pem", nil)
 	if err != nil {
 		slog.Error("TLS Server returned error: " + err.Error())
