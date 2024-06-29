df358713bc2de2e7335808dae0f6cd52dbd85411
nic <nicolas@nfbush.eu>
2024-06-29 01:07:59 +0100

    fix pem order whoops

--- app/main.go
+++ app/main.go
@@ -15,7 +15,7 @@ func catchall(w http.ResponseWriter, r *http.Request) {
 func main() {
 	fmt.Println("Starting...")
 	http.HandleFunc("/*", catchall)
-	err := http.ListenAndServeTLS(":3000", "key.pem", "cert.pem", nil)
+	err := http.ListenAndServeTLS(":3000", "cert.pem", "key.pem", nil)
 	if err != nil {
 		slog.Error("TLS Server returned error: " + err.Error())
 	}
